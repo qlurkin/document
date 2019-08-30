9a7630c04dee0831822da0f1703e3f3c82afb2d9
Quentin Lurkin <qlurkin@gmail.com>
2019-08-30 13:30:36 +0200

    Use rollup for build

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve'
 import postcss from 'rollup-plugin-postcss'
 import babel from 'rollup-plugin-babel'
+const url = require("postcss-url")
 
 export default [
 	{
