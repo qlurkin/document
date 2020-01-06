8fef62c06f83a34a5a5e7a81b4e911066aa1ddf7
Quentin Lurkin <qlurkin@gmail.com>
2020-01-06 16:00:09 +0100

    Simplify

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve'
-import postcss from 'rollup-plugin-postcss'
 import babel from 'rollup-plugin-babel'
-const importUrl = require('postcss-import-url');
-const url = require("postcss-url")
-
-const urlOptions = [
-	{
-		url: 'inline'
-	}
-]
 
 export default [
-	// {
-	// 	input: 'document.js',
-	// 	output: {
-	// 		file: 'dist/document.js',
-	// 		format: 'iife'
-	// 	},
-	// 	plugins: [ 
-	// 		resolve(),
-	// 		babel({
-	// 			exclude: 'node_modules/**'
-	// 		}),
-	// 		postcss({
-	// 			plugins: [importUrl({modernBrowser: true}), url({url: 'inline'})]
-	// 		})
-	// 	]
-	// },
 	{
 		input: 'document.js',
 		output: {
